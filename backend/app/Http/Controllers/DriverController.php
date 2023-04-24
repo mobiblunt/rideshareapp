2dc1affb60ebc0b00734828c266f1f0a42e238c0
Chima Ejiofor <mobiblunt@gmail.com>
2023-04-24 12:10:06 +0100

    fixed auth

---
+++ backend/app/Http/Controllers/DriverController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class DriverController extends Controller
+{
+    public function show(Request $request) {
+        $user = $request->user();
+        $user->load('driver');
+
+        return $user;
+    }
+
+
+    public function update(Request $request) {
+
+        $request->validate([
+            'year' => 'required|numeric|between:2010,2024',
+            'make' => 'required',
+            'model' => 'required',
+            'color' => 'required|alpha',
+            'license_plate' => 'required',
+            'name' => 'required',
+        ]);
+
+        $user = $request->user();
+        $user->update($request->only('name'));
+
+    }
+}
--- backend/app/Http/Controllers/LoginController.php
+++ backend/app/Http/Controllers/LoginController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Notifications\LoginNeedsVerification;
+use App\Models\User;
 
 class LoginController extends Controller
 {
@@ -29,6 +30,25 @@ class LoginController extends Controller
 
 
     public function verify(Request $request) {
+
+        $request->validate([
+            'phone' => 'required|numeric|min:10',
+            'login_code' => 'required|numeric|between:111111,999999'
+        ]);
+
+        $user = User::where('phone', $request->phone)
+            ->where('login_code', $request->login_code)
+            ->first();
+
+        if($user) {
+
+            $user->update([
+                'login_code' => null
+            ]);
+            return $user->createToken($request->login_code)->plainTextToken;
+        }
+
+        return response()->json(['Invalid verification code', 401]);
         
     }
 }
