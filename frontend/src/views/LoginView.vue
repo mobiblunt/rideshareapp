604e501381577e99a22efa2a11417e94fa3fb79e
Chima Ejiofor <mobiblunt@gmail.com>
2023-04-27 12:55:32 +0100

    working on frontend

---
+++ frontend/src/views/LoginView.vue
@@ -0,0 +1,25 @@
+<script setup>
+import {vMaska} from 'maska'
+</script>
+
+
+<template>
+    <div class="pt-16">
+        <h1 class="text-3xl font-semi-bold mb-4">Enter your phone number</h1>
+        <form action="#" @submit="handleLogin">
+            <div class="overflow-hidden shadow sm:rounded-md max-w-sm mx-auto text-left">
+                <div class="bg-white px-4 py-5 sm:p-6">
+                    <div>
+                        <input type="text" v-maska data-maska="# (###) ###-####" name="phone" id="phone" class="mt-1 block w-full px-3 py-2 rounded-md border border-gray-300 shadow-sm">
+                    </div>
+                </div>
+                <div class="bg-gray-50 px-4 py-3 text-right sm:px-6">
+                    <button type="submit" class="inline-flex justify-center rounded-md border border-transparent bg-black py-2 text-white px-3">Continue</button>
+                </div>
+            </div>
+
+
+        </form>
+    </div>
+</template>
+
